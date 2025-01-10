c41704e6c176f6b9b5c5bfe59e89362149f8f637
Manoj Rana <mrana3537@gmail.com>
2025-01-10 17:21:07 +0530

    Optional Database Connection Comment Added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { Express } from "express";
 import cors from "cors";
 
-import sequelize from "./database/db";
+// **** Uncomment This Imoprt To Get Database Connection ****
+// import sequelize from "./database/db";
 import config from "../config/config";
 
 (async () => {
-  await sequelize.sync();
+  // **** Uncomment This Line To Sync With Database ****
+  // await sequelize.sync();
   console.log("****** Environment:[", config.env, "] ***********");
 })();
 const port = config.port;
@@ -16,11 +18,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.status(200).send("API is working fine");
+  res.status(200).send("API is working!!!");
 });
 
-sequelize.authenticate().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+// **** Uncomment this Function To Authenticate With Database Before Running The Application ****
+
+// sequelize.authenticate().then(() => {
+//   app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+//   });
+// });
+
+// **** Remove this app.listen method after uncommenting sequelize.authenticate method ****
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
